eaf32fe2c387f301aa2782e2e3d321337eb5dd47
Thomas Grainger <thomasgrainger@graingert.co.uk>
2014-09-21 11:15:44 +0100

    First try

---
+++ index.sh
@@ -0,0 +1,122 @@
+devpi and PyPI
+
+
+Thomas Grainger
+@graingert
+
+
+Nothing to do with the devpi project, I just think it\'s cool.
+
+pip install devpi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Have you ever seen?
+
+(dev)graingert@adhominem:~/projects$ pip install django
+Downloading/unpacking django
+  Cannot fetch index base URL https://pypi.python.org/simple/
+  Could not find any downloads that satisfy the requirement django
+Cleaning up...
+No distributions at all found for django
+Storing debug log for failure in /home/graingert/.pip/pip.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     devpi: a fast PyPI mirror.
+
+
+     Installed on your Laptop?
+        isolates you from the network.
+        improved speed with caching.
+
+         Installed on a webserver?
+             your own inheritance graph.
+             
